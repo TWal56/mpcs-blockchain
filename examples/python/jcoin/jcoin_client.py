cac9aff6870b3ac0b2a69aeb8aab2e82250be5db
Todd Walters <twalters@Todds-MacBook-Air.local>
2021-12-13 00:22:45 -0500

    jcoin latest

--- examples/python/jcoin/jcoin_client.py
+++ examples/python/jcoin/jcoin_client.py
@@ -14,9 +14,7 @@
 
 from __future__ import print_function
 from concurrent import futures
-
 import logging
-
 import grpc
 import jcoin_pb2
 import jcoin_pb2_grpc
@@ -28,7 +26,7 @@ from collections import OrderedDict
 # confirm node has registered with DNS_SEED
 # then launch blockchain software and start listening...
 # listen for handshakes from new peers, add to list of known peers, return list of all known peers to calling Node
-# also listen for new transactions!?
+# also listen for new transactions...and blocks...
 
 ##########
 # Launch a new node and register with registrar server
@@ -40,12 +38,17 @@ def launch_node():
     channel = grpc.insecure_channel('172.17.0.2:50051')
     stub = jcoin_pb2_grpc.RegistrarStub(channel)
     response = stub.Register(jcoin_pb2.NewNode(nVersion='1', nTime=launch_time, addrMe=node_ip))
-    print("JCoin registrar server says last node added = " + response.lastNodeIp)
+    print('\nJCoin registrar server says last node added = ' + response.lastNodeIp)
+    if not response.lastNodeIp == 'NULL':
+        cur_time = str(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+        Handshake.known_peers[response.lastNodeIp] = {'nTime': cur_time,
+                                                      'nVersion': '1',
+                                                      'bestHeight': 0}
     return response.lastNodeIp
 
 
 ##########
-# Handshake server
+# Handshake server to process inbound shakes
 ##########
 
 class Handshake(jcoin_pb2_grpc.HandshakeServicer):
@@ -54,18 +57,20 @@ class Handshake(jcoin_pb2_grpc.HandshakeServicer):
     node_ip = socket.gethostbyname(socket.gethostname())
 
     def Shake(self, request, context):
-        # use extend to add multiple known peer IPs to handshake response
+        # use extend to add known peer IPs to handshake response
         if request.addrMe not in self.known_peers:
             self.known_peers[request.addrMe] = {'nTime': request.nTime,
                                                 'nVersion': request.nVersion,
                                                 'bestHeight': request.bestHeight}
         else:
             print('node ' + request.addrMe + ' is already known!')
-        print('Known Peers = ' + str(self.known_peers))
-        r_shake_peers = jcoin_pb2.ReceiverShake().knownPeers
-        r_shake_peers.extend(list(self.known_peers))
-        print('Known Peers EXTENDED for RPC response = ' + str(r_shake_peers))
-        return jcoin_pb2.ReceiverShake(knownPeers=r_shake_peers)
+        print('\nNew Inbound Shake from ' + request.addrMe + ' ...')
+        print('\nKnown Peers now = ' + str(self.known_peers))
+        # r_shake_peers = jcoin_pb2.ReceiverShake().knownPeers
+        # r_shake_peers.extend(list(self.known_peers))
+        # print('\nKnown Peers EXTENDED for RPC response = ' + str(r_shake_peers))
+        # return jcoin_pb2.ReceiverShake(knownPeers=r_shake_peers)
+        return jcoin_pb2.ReceiverShake(knownPeers=self.known_peers)
 
 
 def serve_handshake(lastIp):
@@ -79,32 +84,23 @@ def serve_handshake(lastIp):
     jcoin_pb2_grpc.add_HandshakeServicer_to_server(Handshake(), server)
     server.add_insecure_port(socket.gethostbyname(socket.gethostname()) + ':58333')
     server.start()
-    print('Handshake Server is live on ' + node_ip + ':58333')
+    print('\nHandshake Server is live on ' + node_ip + ':58333')
     server.wait_for_termination()
 
 
 ##########
-# Handshake call
+# Handshake call outbound
 ##########
 
-def shake_hands(lastIp):
+def shake_hands(ip_add):
     current_time = str(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
     node_ip = socket.gethostbyname(socket.gethostname())
-    channel = grpc.insecure_channel(lastIp + ':58333')
+    channel = grpc.insecure_channel(ip_add + ':58333')
     stub = jcoin_pb2_grpc.HandshakeStub(channel)
-    # add dynamic bestHeight
     # shake hands with any nodes not yet shaken with
     # add flag to track handshakes
     response = stub.Shake(jcoin_pb2.CallerShake(nVersion='1', nTime=current_time, addrMe=node_ip, bestHeight=0))
-    peers_list = []
-    for peer in response.knownPeers:
-        peers_list.append(peer)
-        # add dynamic bestHeight???
-        if peer not in Handshake.known_peers and peer != node_ip:
-            Handshake.known_peers[peer] = {'nTime': str(datetime.datetime.now().strftime("%Y%m%d-%H%M%S")),
-                                           'nVersion': '1',
-                                           'bestHeight': 0}
-    print("JCoin server says known peers from called Node = " + str(peers_list))
+    return response.knownPeers
 
 
 ##########
@@ -113,13 +109,24 @@ def shake_hands(lastIp):
 
 if __name__ == '__main__':
     logging.basicConfig()
+    node_ip = socket.gethostbyname(socket.gethostname())
     lastNodeIp = launch_node()
     if lastNodeIp != "NULL":
-        print('lastNodeIp not NULL')
-        shake_hands(lastNodeIp)
+        print('\nlastNodeIp not NULL')
+        peers = shake_hands(lastNodeIp)
+        for peer in peers:
+            # peers_list.append(peer)
+            # add dynamic bestHeight???
+            if peer not in Handshake.known_peers and peer != node_ip:
+                print('\nAdding new peer to this node: ' + str(peer))
+                Handshake.known_peers[peer] = {'nTime': str(datetime.datetime.now().strftime("%Y%m%d-%H%M%S")),
+                                               'nVersion': '1',
+                                               'bestHeight': 0}
+                print('\nFor Loop--All known peers currently on this node = ' + str(list(Handshake.known_peers)))
+                shake_hands(peer)
+        print('\nAll known peers currently on this node = ' + str(list(Handshake.known_peers)))
     else:
-        print('lastNodeIp IS NULL')
+        print('\nlastNodeIp IS NULL')
     serve_handshake(lastNodeIp)
-
 
 ###
